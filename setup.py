aa7d0cf5d91841b10b82e35a35ee4b54a01ae233
Satoshi Amemiya <satoshi_amemiya@voyagegroup.com>
2014-06-04 11:00:49 +0900

    0.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name = 'karakuri',
-    version = '0.0.2',
+    version = '0.1.0',
     author = 'Satoshi Amemiya',
     author_email = 'satoshi_amemiya@voyagegroup.com',
     description = 'Task management tool for docker image',
